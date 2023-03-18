be60bfa02e6efb7d3396fc1e02341f7b0f88c429
Asana <asanaalhassanat98@mail.com>
2023-03-18 04:07:02 -0700

    addede betty style documentation

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *Description: code to check if a value is positive,negative or neutral.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
