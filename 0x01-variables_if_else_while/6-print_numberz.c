272225a67850e4abb1cb109a8b82ced6ac22e029
Asana <asanaalhassanat98@mail.com>
2023-03-20 19:35:56 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+for (int i = 0; i < 10; i++)
+{
+putchar('0', i);
+}
+putchar('\n');
+return (0);
+}
