4b43d79fd31734337f4db8617175a2ef042e5638
Asana <asanaalhassanat98@mail.com>
2023-03-20 17:57:30 -0700

    q3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+
+{
+int x = 'A'
+
+while (x <= 'Z')
+{
+char lower = tolower(x);
+putchar (lower);
+x++;
+}
+
+
+while (x <= 'Z')
+{
+char upper = toupper(x);
+putchar (upper);
+x++;
+}
+putchar ('\n');
+return (0);
+}
