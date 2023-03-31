ae05ee2a813e3530e4bb50017b95a41f06fea2fd
Asana <asanaalhassanat98@mail.com>
2023-03-31 07:42:13 -0700

    concat 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,7 +16,10 @@ for (i = 0; dest[i] != '\0'; i++)
 destlen++;
 for (i = 0; src[i] != '\0'; i++)
 srclen++;
-for (i = 0; i < n; i++)
+for (i = 0; i < n && src[i] != '\0'; i++)
 dest[destlen + i] = src[i];
+if (i < n){
+dest[i] = '\0';
+}
 return (dest);
 }
