a4e3ac8789e4e14c0543a57b15f3c23b1d733864
Asana <asanaalhassanat98@mail.com>
2023-03-31 22:58:36 -0700

    q5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,14 +5,13 @@
  *input: string
  * Return: string
  */
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
 int i;
-char str;
 for (i = 0; str[i] != '\0'; i++)
 {
 if (str[i] >= 'a' && str[i] <= 'z')
-str[i] = str[i] = 32;
+str[i] = str[i]-32;
 }
 return (str);
 }
