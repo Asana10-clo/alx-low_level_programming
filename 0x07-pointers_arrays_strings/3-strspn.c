f0f8561a4f4996be74c1466ff080aa7e0ecaec23
Asana <asanaalhassanat98@mail.com>
2023-04-04 18:06:25 -0700

    search bytes

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strspn - gets length of prefix substring
+ * @s: string
+ * @accept: bytes string
+ * Return: bytes from accept
+ */ 
+unsigned int _strspn(char *s, char *accept)
+{       
+        
+}       
+~ 
