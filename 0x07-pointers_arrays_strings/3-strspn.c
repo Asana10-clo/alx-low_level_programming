31156a9b7f048825eee5d6b3bfdbad144458395e
Asana <asanaalhassanat98@mail.com>
2023-04-05 04:50:56 -0700

    length of substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,9 +5,23 @@
  * @s: string
  * @accept: bytes string
  * Return: bytes from accept
- */ 
+ */
 unsigned int _strspn(char *s, char *accept)
-{       
-        
-}       
-~ 
+{
+unsigned int p;
+int q;
+while (*s)
+{
+for (q = 0; accept[q]; q++)
+{
+if (*s == accept[q])
+{
+	p++;
+}
+else if (accept[q + 1] == '\0')
+return (p);
+}
+s++;
+}
+return (p);
+}
