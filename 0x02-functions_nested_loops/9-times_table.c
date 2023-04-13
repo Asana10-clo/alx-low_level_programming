511f3b16e89f2ac0e96b8b13b17bdd548286c626
Asana <asanaalhassanat98@mail.com>
2023-04-12 23:22:44 -0700

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 times table
+ * Return: multiples
+ */
+void times_table(void)
+{
+int i; 
+int n;
+int p;
+for (i = 0; i <= 9; i++)
+{
+_putchar('0');
+for (n = 1; n <= 9; n++)
+{
+_putchar(',');
+_putchar(' ');
+p = i * n;
+if (p <= 9)
+_putchar(' ');
+else
+_putchar((p / 10) + '0');
+_putchar((p % 10) + '0');
+}
+_putchar('\n');
+}
+}
