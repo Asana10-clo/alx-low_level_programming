920423021706c3eeae8cceeb7fbba6fa353a7164
Asana <asanaalhassanat98@mail.com>
2023-04-13 01:14:36 -0700

    check if uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if uppercase
+ * @c: character
+ * Return: result
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+return (1);
+else
+return (0);
+}
