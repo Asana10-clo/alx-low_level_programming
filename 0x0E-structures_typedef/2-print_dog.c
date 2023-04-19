dec5f82fcfac8c1c9bd984d8ac1c5000ba959cab
Asana <asanaalhassanat98@mail.com>
2023-04-18 18:28:51 -0700

    dog struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,6 +10,8 @@ void print_dog(struct dog *d)
 {
 if (d)
 {
-(*d) = name ? (printf("Name: %s\n", (*d) = name)):(printf("Name: (nil)\n"));
-printf("Age: %s\n", (*d) = age);
-(*d) = Owner ? (printf("Owner: %s\n", (*d) = Owner)):(printf("Owner: (nil)\n"));
+(*d).name ? (printf("Name: %s\n", (*d).name)):(printf("Name: (nil)\n"));
+printf("Age: %f\n", (*d).age);
+(*d).owner ? (printf("Owner: %s\n", (*d).owner)):(printf("Owner: (nil)\n"));
+}
+}
