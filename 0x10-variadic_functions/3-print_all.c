1ae26beb5bdca70317f9adfab71111ca70038446
Asana <asanaalhassanat98@mail.com>
2023-04-21 09:09:51 -0700

    print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - anything
+ * @char: character
+ * @format: any string type
+ * Return: anything success
+ */
+void print_all(const char * const format, ...)
+{
+va_list things;
+int i = 0;
+char *str;
+char  *par = " ";
+va_start(things, format);
+if (format)
+{
+while (format[i])
+{
+switch (format[i])
+{
+case 'c':
+printf("%c%s", va_arg(things, int), par);
+break;
+case 'i':
+printf("%d%s", va_arg(things, int), par);
+break;
+case 'f':
+printf("%f%s", va_arg(things, double), par);
+break;
+case 's':
+str = va_arg(things, char *);
+if (str == NULL)
+printf("(nil)");
+else
+printf("%s%s", str, par);
+break;
+default:
+i++;
+continue;
+}
+par = ", ";
+i++;
+}
+}
+printf("\n");
+va_end(things);
+}
