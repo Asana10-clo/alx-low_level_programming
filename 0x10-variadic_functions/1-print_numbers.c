649d06e3f7312dd7992a2e32e42502338f816b54
Asana <asanaalhassanat98@mail.com>
2023-04-20 20:48:27 -0700

    numbs

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ va_start(as, n);
 for (i = 0; i < n; i++)
 {
 printf("%d", va_arg(as, int));
-if (separator != NULL)
+if (i != (n - 1) && separator != NULL)
 printf("%s", separator);
 }
 printf("\n");
