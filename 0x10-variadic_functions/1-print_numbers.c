a475e3a8a63ba73300f9845b89553d45a7f0111c
Asana <asanaalhassanat98@mail.com>
2023-04-20 19:50:16 -0700

    prints numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - functions that prints numbers
+ * @separator: string tobe printed between numbers
+ * @n: params
+ * Return: numbers
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list as;
+unsigned int i;
+va_start(as, n);
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(as, int));
+if (separator != NULL)
+printf("%s", separator);
+}
+printf("\n");
+va_end(as);
+}
