734c81690880ea087a92ea0c74f138a9a851a24b
Asana <asanaalhassanat98@gmail.com>
2023-06-15 07:30:44 -0700

    calloc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: input
+ * @s2: input 2
+ * @n: params
+ * Return: string pointer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+void *q;
+
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory using calloc
+ * @nmemb: array
+ * @size: size of array
+ * Return: succes
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+int *ptr;
+unsigned int i;
+
+if (nmemb == 0 || size == 0)
+return (NULL);
+ptr = malloc(size * nmemb);
+if (ptr == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < nmemb; i++)
+ptr[i] = 0;
+return (ptr);
+}
